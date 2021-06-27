cd518fc238a22d8de24942e2bf91344ef5fe2499
Shamim Akhtar <shamim.akhtar@gmail.com>
2021-06-27 11:17:39 +0800

    A Generic FSM

---
+++ FSM/FiniteStateMachine.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns
+{
+    public class FiniteStateMachine<T>
+    {
+        protected Dictionary<T, State<T>> mStates;
+        protected State<T> mCurrentState;
+
+        public FiniteStateMachine()
+        {
+            mStates = new Dictionary<T, State<T>>();
+        }
+
+        public void Add(State<T> state)
+        {
+            mStates.Add(state.ID, state);
+        }
+
+        public void Add(T stateID, State<T> state)
+        {
+            mStates.Add(stateID, state);
+        }
+
+        public State<T> GetState(T stateID)
+        {
+            return mStates[stateID];
+        }
+
+        public void SetCurrentState(T stateID)
+        {
+            State<T> state = mStates[stateID];
+            SetCurrentState(state);
+        }
+
+        public State<T> GetCurrentState()
+        {
+            return mCurrentState;
+        }
+
+        public void SetCurrentState(State<T> state)
+        {
+            if (mCurrentState == state)
+            {
+                return;
+            }
+
+            if (mCurrentState != null)
+            {
+                mCurrentState.Exit();
+            }
+
+            mCurrentState = state;
+
+            if (mCurrentState != null)
+            {
+                mCurrentState.Enter();
+            }
+        }
+
+        public void Update()
+        {
+            if (mCurrentState != null)
+            {
+                mCurrentState.Update();
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (mCurrentState != null)
+            {
+                mCurrentState.FixedUpdate();
+            }
+        }
+    }
+}
---
+++ FSM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
